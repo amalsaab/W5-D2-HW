cd7c735fe010113e9eba9dd8add8655620a8b31e
amalsaab <441102683@student.ksu.edu.sa>
2023-11-07 12:52:21 +0300

    replace img

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 let type = document.getElementsByClassName("type")
-type = [...type];
+type = [...type]; //convert to array
 let dog = document.getElementById("dog")
 let cat = document.getElementById("cat")
+let image = document.getElementById("image")
 // type.innerHTML = 'cat'
 // console.log(type)
 cat.addEventListener('click', () => {
+    image.src = 'https://images.unsplash.com/photo-1554310212-e1d9f0d4d9be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     type.map((item) => item.innerHTML = 'cat')
 })
 dog.addEventListener('click', () => {
+    image.src = 'https://images.everydayhealth.com/images/lung-respiratory/allergies/best-and-worst-dog-breeds-for-allergies-01-1440x810.jpg'
     type.map((item) => item.innerHTML = 'dog')
 })
